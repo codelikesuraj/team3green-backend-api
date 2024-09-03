41766855fdcd28bdc3d8eda1b7e5cb3b63b023d2
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-09-03 23:00:30 +0100

    update: added Admin middleware and alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
